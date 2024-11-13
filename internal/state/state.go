db3c56ab906a064c16916a3fa98540a235c54128
zach <zliousas@gmail.com>
2024-11-13 22:09:58 +0200

    window prog

--- internal/state/state.go
+++ internal/state/state.go
@@ -1,10 +1,20 @@
 package state
 
+import(
+  "fyne.io/fyne/v2"
+	"fmt"
+)
 
 type State struct {
 	Mode string
-	Args []string
+	Data [2]string
 	File string
 	FileType string
 
+}
+
+
+
+func (s *State) Callback(position fyne.Position, uri []fyne.URI) {
+  fmt.Println(uri[0])
 }
--- internal/window/window.go
+++ internal/window/window.go
@@ -8,6 +8,7 @@ import (
   "fyne.io/fyne/v2"
   "fyne.io/fyne/v2/layout"
   "github.com/justfancy64/pwmng/internal/state"
+  "github.com/justfancy64/pwmng/internal/encoding"
   "os"
 
 )
@@ -36,7 +37,7 @@ func InputWindow(s *state.State) fyne.App {
   content := container.New(layout.NewVBoxLayout(), headercontainer,inputline,confirm) // vertical container input line will be added here
   w.SetContent(content)
 
-  w.SetOnDropped(callback)
+  w.SetOnDropped(s.Callback)
   w.Show()
 
   return a
@@ -50,15 +51,19 @@ func ModeWindow(s *state.State,a fyne.App) {
 	//a := app.New()
 	w := a.NewWindow("Hello")
 
-	hello := widget.NewLabel("use encode to if u want to add a password or decode to retrieve one")
+	hello := widget.NewLabel("Use encode to if u want to add a password or decode to retrieve one")
 	headercontainer := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), hello,layout.NewSpacer())
-	button := container.NewVBox(widget.NewButton("encode", func(){s.Mode = "encode"}))
+	button := container.NewVBox(widget.NewButton("encode", func(){
+		  s.Mode = "encode"
+		  w.Hide()
+		  EncodingWindow(s,a)
+		  }))
 	button2 := container.NewVBox(widget.NewButton("decode", func(){s.Mode = "decode"}))
 	content := container.New(layout.NewVBoxLayout(), headercontainer,button,button2)
 	w.SetContent(content)
 
 
-        w.SetOnDropped(callback)
+        w.SetOnDropped(s.Callback)
 
 	w.Show()
 }
@@ -66,7 +71,29 @@ func ModeWindow(s *state.State,a fyne.App) {
 
 
 
-func callback(position fyne.Position, uri []fyne.URI) {
-  fmt.Println(uri[0])
 
+
+func EncodingWindow(s *state.State,a fyne.App) {
+  w := a.NewWindow("Encoding Window")
+  title := widget.NewLabel("Enter the data u wish to encode")
+  titlecont := container.New(layout.NewHBoxLayout(), layout.NewSpacer(),title,layout.NewSpacer())
+  inputComment := widget.NewEntry()
+  inputComment.SetPlaceHolder("eg: Username and password for www.examplesite.com")
+  inputPassword := widget.NewEntry()
+  inputPassword.SetPlaceHolder("Tip: enter multile entries seperated by spaces")
+  Confirm := widget.NewButton("Confirm", func(){
+    data := [2]strings{inputComment.Text,inputPassword.Text}
+    s.Data = data
+    encoding.StartEncoding(s)
+     
+  })
+
+
+
+  content := container.New(layout.NewVBoxLayout(),titlecont,inputComment,inputPassword)
+
+
+  w.SetContent(content)
+  w.Show()
+  
 }
